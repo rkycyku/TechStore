46dfbde0bb652d70440658a777be43678babfc26
Rilind Kyçyku <r.kycyku.12@gmail.com>
2023-01-29 02:08:39 +0100

    rregullim tek pjesa e porosive

--- php/funksione/fatura.php
+++ php/funksione/fatura.php
@@ -12,7 +12,7 @@ $porosiaCRUD->setPorosiaID($_GET['nrPorosis']);
 
 $porosia = $porosiaCRUD->shfaqPorosinSipasID();
 
-if ($porosia['userID'] != $_SESSION['userID'] || $_SESSION['aksesi'] == 0) {
+if ($porosia['userID'] != $_SESSION['userID'] && $_SESSION['aksesi'] == 0) {
   echo '<script>document.location="../userPages/porosit.php"</script>';
 }
 ?>
